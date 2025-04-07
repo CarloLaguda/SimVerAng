6ce9b4e40822276b3b6cfd0648a21d494ac73322
Carlo <159021623+CarloLaguda@users.noreply.github.com>
2025-04-07 14:51:50 +0000

    punto 4

--- mail-laguda/src/app/app.component.ts
+++ mail-laguda/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SentMailComponent } from './sent-mail/sent-mail.component';
-import { Sentemail } from './sent-mail/sent-email.mode';
+import { Sentemail } from './sent-mail/sent-email.model';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, SentMailComponent],
@@ -17,5 +17,16 @@ export class AppComponent {
       new Sentemail('gianng@gmail.com', 'arrosto', "ayayayaay"),
       new Sentemail('gio@gmail.com', 'anh', "nagaga")
     ];
+  }
+
+  sendEmail(testo: HTMLInputElement, oggetto: HTMLInputElement, email: HTMLInputElement){
+    console.log("ciao")
+    console.log("Mail " + email.value + " Oggetto " + oggetto.value + " Testo "+ testo.value)
+    this.mails.push(new Sentemail(email.value, oggetto.value, testo.value));
+    console.log(this.mails)
+    email.value = ""
+    oggetto.value = ""
+    testo.value = ""
+    return false;
   }
 }
