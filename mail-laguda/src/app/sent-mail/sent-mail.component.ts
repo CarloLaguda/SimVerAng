0453db948c6d639555e34827d8ccdabf101dca8c
Carlo <159021623+CarloLaguda@users.noreply.github.com>
2025-04-08 17:26:18 +0000

    punto 8bb

--- mail-laguda/src/app/sent-mail/sent-mail.component.ts
+++ mail-laguda/src/app/sent-mail/sent-mail.component.ts
@@ -1,31 +1,33 @@
-import { Component, HostBinding, Input } from '@angular/core';
+import { Component, HostBinding, Input, OnInit} from '@angular/core';
 import { Sentemail } from './sent-email.model';
 @Component({
   selector: 'app-sent-mail',
   imports: [],
   templateUrl: './sent-mail.component.html',
-  styleUrl: './sent-mail.component.css'
+  styleUrl: './../app.component.css'
 })
-export class SentMailComponent {
+export class SentMailComponent implements OnInit {
   @Input() email:Sentemail;
-  //@HostBinding('attr.class') cssClass = 'bg-info-subtle';
+  @HostBinding('attr.class') cssClass = 'bg-bianco';
   button_clicked = true
   constructor(){
     this.email = new Sentemail("", "", "")
   }
-  /*
-  isSpecialClicked: boolean = false
-  @HostBinding('class.bg-info') get isBlueBackground() {
-    return this.isSpecialClicked;
-  }
+  
   toggleSpecial() {
-    this.isSpecialClicked = !this.isSpecialClicked;
-    console.log(this.isSpecialClicked)
+    if(this.cssClass == "bg-bianco"){
+      this.cssClass = "bg-giallo"
+      console.log("ciaso1")
+    }else{
+      this.cssClass = "bg-bianco"
+      console.log("ciaso2")
+    }
+    return false;
   }
-  */
+  
   clicked_button(){
     this.email.clicked_button()
     return false;
   }
-
+  ngOnInit(){}
 }
