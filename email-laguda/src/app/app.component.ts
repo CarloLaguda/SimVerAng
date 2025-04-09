c2ea0f85d4619e9a792bdeaf0fba58f16d95a208
Carlo <159021623+CarloLaguda@users.noreply.github.com>
2025-04-09 17:51:59 +0000

    punto 4 bis

--- email-laguda/src/app/app.component.ts
+++ email-laguda/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {Email} from "./email.model";
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,4 +9,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'email-laguda';
+  emails:Email[]
+
+  constructor(){
+    this.emails = [
+      new Email("gigio@gmail.com", "patate", "Ecco la ricetta delle patate"),
+      new Email("giogio@gmail.com", "arrosto", "Ecco la ricetta dell'arrosto"),
+      new Email("giagio@gmail.com", "orata", "Ecco la ricetta dell'orata")
+    ]
+  }
+
+  aggiungiMail(email: HTMLInputElement, oggetto: HTMLInputElement, testo: HTMLInputElement){
+    this.emails.push(new Email(email.value, oggetto.value, testo.value))
+    console.log("elemento aggiunto")
+    email.value = ""
+    oggetto.value = ""
+    testo.value = ""
+  }
 }
