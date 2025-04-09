a1118053cb91848da6d998e04db7b998f59964c3
Carlo <159021623+CarloLaguda@users.noreply.github.com>
2025-04-09 19:12:54 +0000

    punto 6 bis

--- email-laguda/src/app/sent-mail/sent-mail.component.ts
+++ email-laguda/src/app/sent-mail/sent-mail.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Email} from "./email.model";
+
 @Component({
   selector: 'app-sent-mail',
   imports: [],
@@ -8,10 +9,17 @@ import {Email} from "./email.model";
   styleUrl: './sent-mail.component.css'
 })
 export class SentMailComponent {
-  @Input() mail:Email
+  @Input() singola_mail:Email
+  buttone_clk: boolean = true;
+  txt_bottone: string = "chiudi"
 
   constructor(){
-    this.mail = new Email("", "", "")
+    this.singola_mail = new Email("", "", "")
   }
 
+  chiudi_espandi(){
+    this.buttone_clk = !this.buttone_clk 
+    this.txt_bottone = this.buttone_clk ? "Espandi" : "Chiudi"
+    return false
+  }
 }
